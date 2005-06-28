1115febc6d003ad005f7763fd45b7298a3956594
hub <hub>
2005-06-28 01:12:03 +0000

    cleanup in headers

---
+++ include/libopenraw/libopenraw.h
@@ -0,0 +1,23 @@
+
+
+
+
+
+#ifndef __LIBOPENRAW_H__
+#define __LIBOPENRAW_H__
+
+#include <libopenraw/io/io.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+
+#ifdef __cplusplus
+};
+#endif
+
+#endif
