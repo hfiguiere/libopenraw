df535d8923811e574d07c20d0cbd183f764b1d3d
hub <hub>
2005-12-31 03:43:25 +0000

    stupid bugs caught by gcc warnings

--- lib/thumbnails.c
+++ lib/thumbnails.c
@@ -1,4 +1,24 @@
+/*
+ * Copyright (C) 2005 Hubert Figuiere
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
 
+#include <stdlib.h>
+#include <string.h>
 
 #include "thumbnails.h"
 
@@ -34,12 +54,12 @@ or_error openraw_get_extract_thumbnail(const char* filename,
     RawFileRef raw_file;
 
     if (*thumbnail == NULL) {
-        thumbnail = openraw_thumbnail_new();
+        *thumbnail = openraw_thumbnail_new();
     }
 
     raw_file = raw_open(get_default_io_methods(), filename, O_RDONLY);
 
-    cr2_get_thumbnail(raw_file, thumbnail);
+    cr2_get_thumbnail(raw_file, *thumbnail);
 
     raw_close(raw_file);
     raw_file = NULL;
