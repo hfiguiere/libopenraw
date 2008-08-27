51c89f643688582438573fc7ff3044f3db21ead1
Hubert Figuiere <hub@stansmith.figuiere.net>
2008-08-27 11:56:24 -0600

    reindent

--- include/libopenraw++/rawdata.h
+++ include/libopenraw++/rawdata.h
@@ -2,6 +2,7 @@
  * libopenraw - rawdata.h
  *
  * Copyright (C) 2007-2008 Hubert Figuiere
+ * Copyright (C) 2008 Novell, Inc.
  *
  * This library is free software: you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License
@@ -30,51 +31,61 @@
 
 namespace OpenRaw {
 
-	class RawData
-		: public BitmapData
-	{
-	public:
-		typedef or_cfa_pattern CfaPattern;
-		static RawData * getAndExtractRawData(const char* filename, 
-											  uint32_t options,
-											  or_error & err);
+class RawData
+    : public BitmapData
+{
+public:
+    typedef or_cfa_pattern CfaPattern;
+    static RawData * getAndExtractRawData(const char* filename, 
+                                          uint32_t options,
+                                          or_error & err);
 
-		RawData();
-		virtual ~RawData();
+    RawData();
+    virtual ~RawData();
 
-		uint16_t min();
-		uint16_t max();
-		void setMin(uint16_t _m);
-		void setMax(uint16_t _m);		
+    uint16_t min();
+    uint16_t max();
+    void setMin(uint16_t _m);
+    void setMax(uint16_t _m);		
 
-		/** swap the two objects data. */
-		void swap(RawData & with);
+    /** swap the two objects data. */
+    void swap(RawData & with);
 
-		virtual void *allocData(const size_t s);
-		virtual void setDimensions(uint32_t x, uint32_t y);
-		void setCfaPattern(CfaPattern t);
-		CfaPattern cfaPattern();
-		uint32_t compression();
-		void setCompression(uint32_t c);
+    virtual void *allocData(const size_t s);
+    virtual void setDimensions(uint32_t x, uint32_t y);
+    void setCfaPattern(CfaPattern t);
+    CfaPattern cfaPattern();
+    uint32_t compression();
+    void setCompression(uint32_t c);
 
-		void setSlices(const std::vector<uint16_t> & slices);
 
-		/** append a uint8_t at the current position */
+    void setSlices(const std::vector<uint16_t> & slices);
+
+    /** append a uint8_t at the current position */
 //		BitmapData &append(uint8_t c);
-		/** append a uint18_t at the current position */
-		RawData &append(uint16_t c);
-		/** Jump to next row. Take slicing into account. */
-		void nextRow();
-	private:
-		class Private;
-		RawData::Private *d;
+    /** append a uint18_t at the current position */
+    RawData &append(uint16_t c);
+    /** Jump to next row. Take slicing into account. */
+    void nextRow();
+private:
+    class Private;
+    RawData::Private *d;
 
-		/** private copy constructor to make sure it is not called */
-		RawData(const RawData& f);
-		/** private = operator to make sure it is never called */
-		RawData & operator=(const RawData&);
-	};
+    /** private copy constructor to make sure it is not called */
+    RawData(const RawData& f);
+    /** private = operator to make sure it is never called */
+    RawData & operator=(const RawData&);
+};
 
 }
 
+/*
+  Local Variables:
+  mode:c++
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0))
+  indent-tabs-mode:nil
+  fill-column:80
+  End:
+*/
 #endif
--- lib/rawdata.cpp
+++ lib/rawdata.cpp
@@ -2,6 +2,7 @@
  * libopenraw - rawdata.cpp
  *
  * Copyright (C) 2007 Hubert Figuiere
+ * Copyright (C) 2008 Novell, Inc.
  *
  * This library is free software: you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License
@@ -25,219 +26,228 @@
 
 namespace OpenRaw {
 
-	class RawData::Private {
-	public:
-		RawData *self;
-		uint16_t min, max;
-		CfaPattern cfa_pattern;
-		uint32_t compression;
-		uint8_t *pos;
-		size_t offset;
-		size_t row_offset;
-		uint8_t slice; /**< the slice index */
-		uint32_t sliceWidth; /**< the width of the current slice */
-		uint32_t sliceOffset;/**< the offset */
+class RawData::Private {
+public:
+    RawData *self;
+    uint16_t min, max;
+    CfaPattern cfa_pattern;
+    uint32_t compression;
+    uint8_t *pos;
+    size_t offset;
+    size_t row_offset;
+    uint8_t slice; /**< the slice index */
+    uint32_t sliceWidth; /**< the width of the current slice */
+    uint32_t sliceOffset;/**< the offset */
 
-		std::vector<uint16_t> slices; /** all the slice width. */
+    std::vector<uint16_t> slices; /** all the slice width. */
 
-		Private(RawData *_self)
-			:	self(_self), 
-				min(0), max(0),
-				cfa_pattern(OR_CFA_PATTERN_NONE),
-				compression(0),
-				pos(NULL), offset(0),
-				row_offset(0),
-				slice(0), sliceWidth(0),
-				sliceOffset(0), slices()
-			{
-			}
-		void advance(size_t s);
-		void nextSlice();
-		void nextRow();
-	private:
-		Private(const Private &);
-		Private & operator=(const Private &);
-	};
-
-
-	RawData *
-	RawData::getAndExtractRawData(const char* filename, uint32_t options,
-																or_error & err)
-	{
-		err = OR_ERROR_NONE;
-		RawData *rawdata = NULL;
-
-		RawFile *file = RawFile::newRawFile(filename);
-		if (file) {
-			rawdata = new RawData();
-			err = file->getRawData(*rawdata, options);
-			delete file;
-		}
-		else {
-			err = OR_ERROR_CANT_OPEN; // file error
-		}
-		return rawdata;
-	}
+    Private(RawData *_self)
+        :	self(_self), 
+                min(0), max(0),
+                cfa_pattern(OR_CFA_PATTERN_NONE),
+                compression(0),
+                pos(NULL), offset(0),
+                row_offset(0),
+                slice(0), sliceWidth(0),
+                sliceOffset(0), slices()
+        {
+        }
+    void advance(size_t s);
+    void nextSlice();
+    void nextRow();
+private:
+    Private(const Private &);
+    Private & operator=(const Private &);
+};
 
 
-	RawData::RawData()
-		: BitmapData(),
-		  d(new RawData::Private(this))
-	{
+RawData *
+RawData::getAndExtractRawData(const char* filename, uint32_t options,
+                              or_error & err)
+{
+    err = OR_ERROR_NONE;
+    RawData *rawdata = NULL;
 
-	}
-
-
-	RawData::~RawData()
-	{
-		delete d;
-	}
-
-	uint16_t RawData::min()
-	{
-		return d->min;
-	}
-
-	uint16_t RawData::max()
-	{
-		return d->max;
-	}
-
-	void RawData::setMin(uint16_t m)
-	{
-		d->min = m;
-	}
-
-	void RawData::setMax(uint16_t m)
-	{
-		d->max = m;
-	}
-
-	void RawData::swap(RawData & with)
-	{
-		BitmapData::swap(with);
-		std::swap(this->d, with.d);
-	}
-
- 	void * RawData::allocData(const size_t s)
-	{
-		void * p = BitmapData::allocData(s);
-		d->pos = (uint8_t*)p;
-		d->offset = 0;
-		return p;
-	}
+    RawFile *file = RawFile::newRawFile(filename);
+    if (file) {
+        rawdata = new RawData();
+        err = file->getRawData(*rawdata, options);
+        delete file;
+    }
+    else {
+        err = OR_ERROR_CANT_OPEN; // file error
+    }
+    return rawdata;
+}
 
 
-	void RawData::setDimensions(uint32_t _x, uint32_t _y)
-	{
-		BitmapData::setDimensions(_x, _y);
-		if(d->slices.size()) {
-			d->sliceWidth = d->slices[0];
-		}
-		else {
-			d->sliceWidth = _x;
-		}
-	}
-
-	void RawData::setSlices(const std::vector<uint16_t> & slices)
-	{
-		d->slices = slices;
-		if(slices.size()) {
-			d->sliceWidth = slices[0];
-		}
-		else {
-			d->sliceWidth = x();
-		}
-	}
-
-	void RawData::setCfaPattern(or_cfa_pattern t)
-	{
-		d->cfa_pattern = t;
-	}
-
-	or_cfa_pattern RawData::cfaPattern()
-	{
-		return d->cfa_pattern;
-	}
-
-	void RawData::setCompression(uint32_t t)
-	{
-		d->compression = t;
-	}
-
-	uint32_t RawData::compression()
-	{
-		return d->compression;
-	}
-
-#if 0
-	RawData &RawData::append(uint8_t c)
-	{
-		assert(d->pos);
-		assert(d->offset < d->data_size);
-		*(d->pos) = c;
-		advance(sizeof(c));
-		return *this;
-	}
-#endif
-
-	RawData &RawData::append(uint16_t c)
-	{
-		assert(d->pos);
-		assert(d->offset < size());
-		*(d->pos) = c & 0xff;
-		*(d->pos + 1) = (c >> 8) & 0xff; 
-		d->advance(sizeof(c));
-		return *this;
-	}
-	
-
-	void RawData::nextRow()
-	{
-		d->nextRow();
-	}
-
-
-	void RawData::Private::nextRow()
-	{
-		uint32_t w = self->x() * 2;
-		uint32_t row = offset / w;
-		row++;
-		if(row == self->y()) 
-		{
-			// on the last
-			nextSlice();
-			row = 0;
-		}
-		offset = row * w + sliceOffset * 2;
-		pos = (uint8_t*)(self->data()) + offset;
-		row_offset = offset;
-	}
-
-	void RawData::Private::nextSlice()
-	{
-		if(slices.size()) {
-			sliceOffset += slices[slice];
-			slice++;
-			if(slices.size() > slice) {
-				sliceWidth = slices[slice];
-			}
-			else {
-				sliceWidth = 0;
-			}
-		}
-	}
-	
-	void RawData::Private::advance(size_t s)
-	{
-		if(offset + s - row_offset >= sliceWidth * 2) {
-			nextRow();
-		}
-		else { 
-			pos += s;
-			offset += s;
-		}
-	}
+RawData::RawData()
+    : BitmapData(),
+      d(new RawData::Private(this))
+{
 
 }
+
+
+RawData::~RawData()
+{
+    delete d;
+}
+
+uint16_t RawData::min()
+{
+    return d->min;
+}
+
+uint16_t RawData::max()
+{
+    return d->max;
+}
+
+void RawData::setMin(uint16_t m)
+{
+    d->min = m;
+}
+
+void RawData::setMax(uint16_t m)
+{
+    d->max = m;
+}
+
+void RawData::swap(RawData & with)
+{
+    BitmapData::swap(with);
+    std::swap(this->d, with.d);
+}
+
+void * RawData::allocData(const size_t s)
+{
+    void * p = BitmapData::allocData(s);
+    d->pos = (uint8_t*)p;
+    d->offset = 0;
+    return p;
+}
+
+
+void RawData::setDimensions(uint32_t _x, uint32_t _y)
+{
+    BitmapData::setDimensions(_x, _y);
+    if(d->slices.size()) {
+        d->sliceWidth = d->slices[0];
+    }
+    else {
+        d->sliceWidth = _x;
+    }
+}
+
+void RawData::setSlices(const std::vector<uint16_t> & slices)
+{
+    d->slices = slices;
+    if(slices.size()) {
+        d->sliceWidth = slices[0];
+    }
+    else {
+        d->sliceWidth = x();
+    }
+}
+
+void RawData::setCfaPattern(or_cfa_pattern t)
+{
+    d->cfa_pattern = t;
+}
+
+or_cfa_pattern RawData::cfaPattern()
+{
+    return d->cfa_pattern;
+}
+
+void RawData::setCompression(uint32_t t)
+{
+    d->compression = t;
+}
+
+uint32_t RawData::compression()
+{
+    return d->compression;
+}
+
+#if 0
+RawData &RawData::append(uint8_t c)
+{
+    assert(d->pos);
+    assert(d->offset < d->data_size);
+    *(d->pos) = c;
+    advance(sizeof(c));
+    return *this;
+}
+#endif
+
+RawData &RawData::append(uint16_t c)
+{
+    assert(d->pos);
+    assert(d->offset < size());
+    *(d->pos) = c & 0xff;
+    *(d->pos + 1) = (c >> 8) & 0xff; 
+    d->advance(sizeof(c));
+    return *this;
+}
+	
+
+void RawData::nextRow()
+{
+    d->nextRow();
+}
+
+
+void RawData::Private::nextRow()
+{
+    uint32_t w = self->x() * 2;
+    uint32_t row = offset / w;
+    row++;
+    if(row == self->y()) 
+    {
+        // on the last
+        nextSlice();
+        row = 0;
+    }
+    offset = row * w + sliceOffset * 2;
+    pos = (uint8_t*)(self->data()) + offset;
+    row_offset = offset;
+}
+
+void RawData::Private::nextSlice()
+{
+    if(slices.size()) {
+        sliceOffset += slices[slice];
+        slice++;
+        if(slices.size() > slice) {
+            sliceWidth = slices[slice];
+        }
+        else {
+            sliceWidth = 0;
+        }
+    }
+}
+	
+void RawData::Private::advance(size_t s)
+{
+    if(offset + s - row_offset >= sliceWidth * 2) {
+        nextRow();
+    }
+    else { 
+        pos += s;
+        offset += s;
+    }
+}
+
+}
+/*
+  Local Variables:
+  mode:c++
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0))
+  indent-tabs-mode:nil
+  fill-column:80
+  End:
+*/
 
