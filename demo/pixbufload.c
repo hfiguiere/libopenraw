e30b604fc4e30e63e1c0b11595eb3cbe2a149fb2
Hubert Figuière <hub@figuiere.net>
2012-06-10 19:59:42 -0700

    Reindent and reorganise code

--- demo/pixbufload.c
+++ demo/pixbufload.c
@@ -36,55 +36,62 @@ static void pixbuf_free(guchar * data, gpointer u)
 int
 main(int argc, char **argv)
 {
+    GdkPixbuf *pixbuf = NULL;
+    ORRawFileRef raw_file = NULL;
     char *filename = argv[1];
-    
+    or_error err;
+    ORBitmapDataRef bitmapdata = NULL;
+
     (void)argc;
     or_debug_set_level(DEBUG2);
     g_type_init();
-    
-    if(filename && *filename)
-    {
-        GdkPixbuf *pixbuf = NULL;
-        ORRawFileRef raw_file = or_rawfile_new(filename, OR_DATA_TYPE_NONE);
-	
-        if(raw_file) {
-            or_error err;
-            ORBitmapDataRef bitmapdata = or_bitmapdata_new();
-            err = or_rawfile_get_rendered_image(raw_file, bitmapdata, 0);
-            if(err == OR_ERROR_NONE) {
-                uint32_t x,y;
-                or_data_type format = or_bitmapdata_format(bitmapdata);
-                x = y = 0;
-                or_bitmapdata_dimensions(bitmapdata, &x, &y);
-                if(format == OR_DATA_TYPE_PIXMAP_8RGB) {
-                    pixbuf = gdk_pixbuf_new_from_data(or_bitmapdata_data(bitmapdata), 
-                                                      GDK_COLORSPACE_RGB,
-                                                      FALSE, 8, x , y , 
-                                                      x * 3, 
-                                                      pixbuf_free, bitmapdata);
-                }
-                else {
-                    /* Gdk pixbuf still suck ass in 2012 not supporting 16bpp. */
-                    printf("16 bits isn't supported because Gdkpixbuf still suck\n");
-                }
-            }
-            or_rawfile_release(raw_file);
-        }
-	
-	
-        if(pixbuf) {
-            gdk_pixbuf_save (pixbuf, "gdk-demosaic.jpg", "jpeg", NULL,
-                             "quality", "100", NULL);
-            g_object_unref(pixbuf);
+
+    if(!filename || !*filename) {
+        printf("No input file name\n");
+        return 1;
+    }
+
+    raw_file = or_rawfile_new(filename, OR_DATA_TYPE_NONE);
+    if(!raw_file) {
+        printf("error loading file %s\n", filename);
+        return 1;
+    }
+
+    bitmapdata = or_bitmapdata_new();
+    err = or_rawfile_get_rendered_image(raw_file, bitmapdata, 0);
+    if(err == OR_ERROR_NONE) {
+        uint32_t x,y;
+        or_data_type format = or_bitmapdata_format(bitmapdata);
+        x = y = 0;
+        or_bitmapdata_dimensions(bitmapdata, &x, &y);
+        if(format == OR_DATA_TYPE_PIXMAP_8RGB) {
+            pixbuf = gdk_pixbuf_new_from_data(or_bitmapdata_data(bitmapdata),
+                                              GDK_COLORSPACE_RGB,
+                                              FALSE, 8, x , y,
+                                              x * 3,
+                                              pixbuf_free, bitmapdata);
         }
         else {
-            printf("error\n");
+            /* Gdk pixbuf still suck ass in 2012 not supporting 16bpp. */
+            printf("16 bits isn't supported because Gdkpixbuf still suck\n");
         }
     }
-    else {
-        printf("No input file name\n");
+    or_rawfile_release(raw_file);
+
+    if(pixbuf) {
+        GError* error = NULL;
+        if(!gdk_pixbuf_save (pixbuf, "gdk-demosaic.jpg", "jpeg", &error,
+                             "quality", "100", NULL) && error) {
+            printf("error saving image: %s\n", error->message);
+            g_error_free(error);
+        }
+        g_object_unref(pixbuf);
     }
-    
+    else {
+        printf("error creating pixbuf\n");
+        return 1;
+    }
+
     return 0;
 }
 /*
