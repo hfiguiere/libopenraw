8f617950cf70ea61df276592137c9223023b5758
Hubert Figuière <hub@figuiere.net>
2014-04-13 08:58:26 -0400

    Re-indent unpack.cpp

--- lib/unpack.cpp
+++ lib/unpack.cpp
@@ -1,4 +1,3 @@
-/* -*- tab-width:4; indent-tabs-mode:'t c-file-style:"stroustrup" -*- */
 /*
  * libopenraw - unpack.cpp
  *
@@ -20,85 +19,96 @@
  * <http://www.gnu.org/licenses/>.
  */
 
-
 #include <assert.h>
 #include "libopenraw/consts.h"
 #include "unpack.h"
 #include "trace.h"
 #include "ifd.h"
 
-namespace OpenRaw {	namespace Internals {
+namespace OpenRaw {
+namespace Internals {
 
-	using namespace Debug;
+using namespace Debug;
 
-	Unpack::Unpack(uint32_t w, uint32_t t)
-		: m_w(w), m_type(t)
-	{
-	}
+Unpack::Unpack(uint32_t w, uint32_t t)
+  : m_w(w), m_type(t)
+{
+}
 
-	/* Return the size of an image row. */
-	size_t Unpack::block_size()
-	{
-		size_t bs;
-		if(m_type == IFD::COMPRESS_NIKON_PACK) {
-			bs = (m_w / 2 * 3) + (m_w / 10);
-		}
-		else {
-			bs = m_w / 2 * 3;
-		}
-		return bs;
-	}
+/* Return the size of an image row. */
+size_t Unpack::block_size()
+{
+  size_t bs;
+  if(m_type == IFD::COMPRESS_NIKON_PACK) {
+    bs = (m_w / 2 * 3) + (m_w / 10);
+  }
+  else {
+    bs = m_w / 2 * 3;
+  }
+  return bs;
+}
 
 
-	/** source is in BE byte order
-	 * the output is always 16-bits values in native (host) byte order.
-	 * the source must correspond to an image row.
-	 */
-	or_error Unpack::unpack_be12to16(uint8_t *dest, size_t destsize, const uint8_t *src,
-								   size_t size, size_t & out)
-	{
-		or_error err = OR_ERROR_NONE;
-		uint16_t *dest16 = reinterpret_cast<uint16_t *>(dest);
-		size_t pad = (m_type == IFD::COMPRESS_NIKON_PACK) ? 1 : 0;
-		size_t n = size / (15 + pad);
-		size_t rest = size % (15 + pad);
-		size_t ret = n * 20 + rest / 3 * 4;
+/** source is in BE byte order
+ * the output is always 16-bits values in native (host) byte order.
+ * the source must correspond to an image row.
+ */
+or_error Unpack::unpack_be12to16(uint8_t *dest, size_t destsize, const uint8_t *src,
+                                 size_t size, size_t & out)
+{
+  or_error err = OR_ERROR_NONE;
+  uint16_t *dest16 = reinterpret_cast<uint16_t *>(dest);
+  size_t pad = (m_type == IFD::COMPRESS_NIKON_PACK) ? 1 : 0;
+  size_t n = size / (15 + pad);
+  size_t rest = size % (15 + pad);
+  size_t ret = n * 20 + rest / 3 * 4;
 
-		/* The inner loop advances 10 columns, which corresponds to 15 input
-		   bytes, 20 output bytes and, in a Nikon pack, one padding byte.*/
-		if (pad) {
-			assert (size % 16 == 0);
-		}
-		assert (rest % 3 == 0);
-		
-		for (size_t i = 0; i < n + 1; i++) {
-			size_t m = i == n ? rest / 3 : 5;
-			if((reinterpret_cast<uint8_t *>(dest16) - dest) + (m * 4) >  destsize) {
-				err = OR_ERROR_DECOMPRESSION;
-//				fprintf(stderr, "overflow !\n");
-				break;
-			}
-			for(size_t j = 0; j < m; j++) {
-				/* Read 3 bytes */
-				uint32_t t = *src++;
-				t <<= 8;
-				t |= *src++;
-				t <<= 8;
-				t |= *src++;
+  /* The inner loop advances 10 columns, which corresponds to 15 input
+     bytes, 20 output bytes and, in a Nikon pack, one padding byte.*/
+  if (pad) {
+    assert (size % 16 == 0);
+  }
+  assert (rest % 3 == 0);
 
-				/* Write two 16 bit values. */
-				*dest16 = (t & (0xfff << 12)) >> 12;
-				dest16++;
+  for (size_t i = 0; i < n + 1; i++) {
+    size_t m = (i == n) ? rest / 3 : 5;
+    if((reinterpret_cast<uint8_t *>(dest16) - dest) + (m * 4) >  destsize) {
+      err = OR_ERROR_DECOMPRESSION;
+      Debug::Trace(ERROR) << "overflow !\n";
+      break;
+    }
+    for(size_t j = 0; j < m; j++) {
+      /* Read 3 bytes */
+      uint32_t t = *src++;
+      t <<= 8;
+      t |= *src++;
+      t <<= 8;
+      t |= *src++;
 
-				*dest16 = t & 0xfff;
-				dest16++;
-			}
+      /* Write two 16 bit values. */
+      *dest16 = (t & (0xfff << 12)) >> 12;
+      dest16++;
 
-			src += pad;
-		}
+      *dest16 = t & 0xfff;
+      dest16++;
+    }
 
-		out = ret;
-		return err;
-	}
+    src += pad;
+  }
+
+  out = ret;
+  return err;
+}
 
 } }
+/*
+  Local Variables:
+  mode:c++
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0))
+  tab-width:2
+  c-basic-offset:2
+  indent-tabs-mode:nil
+  fill-column:80
+  End:
+*/
