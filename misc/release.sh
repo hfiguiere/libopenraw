83607b726ae501ccc335ea6bacb15f43d6abe212
Hubert Figuière <hub@figuiere.net>
2016-11-26 23:02:05 -0500

    misc: added release script

---
+++ misc/release.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+
+DIR=`dirname $0`/..
+
+VERSION=`head -1 $DIR/NEWS | cut -d ' ' -f 2`
+
+if test -z '$VERSION'; then
+    echo "Can't figure out version"
+    exit 1
+fi
+
+if [ ! -f libopenraw-$VERSION.tar.bz2 -o ! -f libopenraw-$VERSION.tar.xz ] ; then
+    make distcheck || exit 2
+fi
+
+echo "Signing packages. You'll be asked for the passphrase"
+
+for f in libopenraw-$VERSION.tar.bz2 libopenraw-$VERSION.tar.xz ; do
+    rm $f.asc
+    gpg -ba $f
+done
+
+
+REMOTE_DEST=annarchy.freedesktop.org:/srv/libopenraw.freedesktop.org/www/download/
+
+scp libopenraw-$VERSION.tar.bz2 libopenraw-$VERSION.tar.bz2.asc annarchy.freedesktop.org:/srv/libopenraw.freedesktop.org/www/download/
