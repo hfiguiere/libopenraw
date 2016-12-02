f31e1f818e9f5748c3fbdfea90ca95dc3f6ef535
Hubert Figuière <hub@figuiere.net>
2016-12-01 21:16:33 -0500

    Bug 98929 - Use proper endian.h for FreeBSD
    Patch by Jason E Hale <jhale@freebsd.org>

--- demo/ppmload.c
+++ demo/ppmload.c
@@ -30,6 +30,8 @@
 # include <machine/endian.h>
 # include <libkern/OSByteOrder.h>
 # define htobe16(x) OSSwapHostToBigInt16(x)
+#elif defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__DragonFly__)
+# include <sys/endian.h>
 #else
 # include <endian.h>
 #endif
