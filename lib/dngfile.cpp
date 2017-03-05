dc52268d19b7d96feae1dc245d31a726534b98d1
Hubert Figuière <hub@figuiere.net>
2017-03-05 15:46:41 -0500

    dng: Added Pentax K70 (DNG)

--- lib/dngfile.cpp
+++ lib/dngfile.cpp
@@ -71,6 +71,8 @@ const IfdFile::camera_ids_t DngFile::s_def[] = {
                                                  OR_TYPEID_PENTAX_K3_DNG) },
     { "PENTAX K-3 II      ", OR_MAKE_FILE_TYPEID(OR_TYPEID_VENDOR_PENTAX,
                                                  OR_TYPEID_PENTAX_K3_II_DNG) },
+    { "PENTAX K-70        ", OR_MAKE_FILE_TYPEID(OR_TYPEID_VENDOR_PENTAX,
+                                                 OR_TYPEID_PENTAX_K70_DNG) },
     { "PENTAX MX-1            ", OR_MAKE_FILE_TYPEID(OR_TYPEID_VENDOR_PENTAX,
                                                  OR_TYPEID_PENTAX_MX1_DNG) },
     { "R9 - Digital Back DMR",   OR_MAKE_FILE_TYPEID(OR_TYPEID_VENDOR_LEICA,
