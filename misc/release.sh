4c6d7d2f4218bc3484eb6e4dd8c25afe015f8adb
Hubert Figuière <hub@figuiere.net>
2020-03-26 21:36:31 -0400

    Update release script helper

--- misc/release.sh
+++ misc/release.sh
@@ -17,11 +17,14 @@ fi
 echo "Signing packages. You'll be asked for the passphrase"
 
 for f in libopenraw-$VERSION.tar.bz2 libopenraw-$VERSION.tar.xz ; do
-    rm $f.asc
+    rm -f $f.asc
     gpg -ba $f
+    echo "File $f ready"
+    echo "Signature $f.asc"
 done
 
+echo "Copy the files above into 'static/download'"
 
-REMOTE_DEST=annarchy.freedesktop.org:/srv/libopenraw.freedesktop.org/www/download/
+# REMOTE_DEST=annarchy.freedesktop.org:/srv/libopenraw.freedesktop.org/www/download/
 
-scp libopenraw-$VERSION.tar.bz2 libopenraw-$VERSION.tar.bz2.asc annarchy.freedesktop.org:/srv/libopenraw.freedesktop.org/www/download/
+# scp libopenraw-$VERSION.tar.bz2 libopenraw-$VERSION.tar.bz2.asc annarchy.freedesktop.org:/srv/libopenraw.freedesktop.org/www/download/
