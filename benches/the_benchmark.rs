4e5a9733c93627df98a98ef1fd640d6f35dbef44
Hubert Figuière <hub@figuiere.net>
2023-02-15 00:17:26 -0500

    Implement benchark

---
+++ benches/the_benchmark.rs
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+/*
+ * libopenraw - the_benchmark.rs
+ *
+ * Copyright (C) 2023 Hubert Figuière
+ *
+ * This library is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#![doc = include_str!("../doc/benchmarks.md")]
+
+const FILES: [&str; 11] = [
+    "Apple/iPhone XS/IMG_1105.dng",
+    "Canon/EOS 10D/CRW_7673.CRW",
+    "Canon/EOS 20D/IMG_3893.CR2",
+    "Canon/Canon EOS R5/Canon_EOS_R5_CRAW_ISO_100_nocrop_nodual.CR3",
+    "Epson/R-D1/_EPS0672.ERF",
+    "Fujifilm/X-Pro1/DSCF2131.RAF",
+    "Leica/M8/L1030132.DNG",
+    "Nikon/D60/DSC_8294.NEF",
+    "Olympus/E-P1/P1080385.ORF",
+    "Pentax/K100D/IMGP1754.PEF",
+    "Sony/ILCE-7RM4/DSC00395.ARW",
+];
+
+use criterion::{criterion_group, criterion_main, Criterion};
+use libopenraw::rawfile_from_file;
+
+pub fn ordiag_benchmark(c: &mut Criterion) {
+    let dataset = std::env::var("RAWFILES_ROOT").expect("RAWFILES_ROOT not set");
+    let dataset = std::path::PathBuf::from(dataset);
+    for file in FILES {
+        let bench_name = format!("ordiag-{}", file);
+        let file = dataset.join(file);
+        c.bench_function(&bench_name, |b| {
+            b.iter(|| {
+                let rawfile = rawfile_from_file(&file, None);
+                if let Ok(rawfile) = rawfile {
+                    let sizes = rawfile.thumbnail_sizes();
+                    for size in sizes {
+                        let thumb = rawfile.thumbnail(size);
+                    }
+                    let raw_data = rawfile.raw_data(false);
+                }
+            })
+        });
+    }
+}
+
+pub fn dump_benchmark(c: &mut Criterion) {
+    let dataset = std::env::var("RAWFILES_ROOT").expect("RAWFILES_ROOT not set");
+    let dataset = std::path::PathBuf::from(dataset);
+    for file in FILES {
+        let bench_name = format!("dump-{}", file);
+        let file = dataset.join(file);
+        c.bench_function(&bench_name, |b| {
+            b.iter(|| {
+                let rawfile = rawfile_from_file(&file, None).expect("RAW didn't decoded");
+                rawfile.dump_file(&mut std::io::sink());
+            });
+        });
+    }
+}
+
+criterion_group!(benches, ordiag_benchmark, dump_benchmark);
+criterion_main!(benches);
---
+++ doc/benchmarks.md
@@ -0,0 +1,15 @@
+How to run the benchmarks
+=========================
+
+Note: these benchmark I used to quantify improvements or regression in
+the code.
+
+## Preparation
+
+Samples come from [Pixl.us RAW repository](https://raw.pixls.us/)
+
+You can rsync the whole repository into `raw-pixls-us-data`.
+
+## Running
+
+`RAWFILES_ROOT` environment need to be set to the `raw-pixls-us-data` directory.
