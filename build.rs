38e760115699a7451a8399f0e1372dba1a8b3ec4
Hubert Figuière <hub@figuiere.net>
2024-01-07 00:00:21 -0500

    cargo: Remove cc dependency
    And remove build script
    
    Signed-off-by: Hubert Figuière <hub@figuiere.net>

--- build.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    //    cc::Build::new()
-    //        .cpp(true)
-    //        .include("./include/")
-    //        .include("./lib/")
-    //        .compile("render");
-    //    println!("cargo:rustc-link-lib=render");
-}
